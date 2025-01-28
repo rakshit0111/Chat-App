da14e58f01d6bbdb251e3d1ea0ff68a735473046
rakshit0111 <rakshitsingh333@gmail.com>
2025-01-29 01:14:22 +0530

    signup working

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,15 @@
-import User from "../models/user.model";
-import bcrypt, { hash } from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req,res) =>{
     const {fullName,email,password} = req.body;
     try {
+        
+        if(!fullName || !password || !email)
+        {
+            return res.status(400).json({message : "Some credential missing"})
+        }
         if(password.length < 6)
         {
             return res.status(400).json({message : "Password length should be more than 6 characters"})
@@ -28,12 +34,22 @@ export const signup = async (req,res) =>{
         if(newUser)
         {
             //JWT Token generation code
+            generateToken(newUser._id,res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id : newUser._id,
+                fullName : newUser.fullName,
+                email : newUser.email,
+                profilePic : newUser.profilePic,
+            })
         }
         else{
             res.status(400).json({message : "Invalid credentials"})
         }
     } catch (error) {
-        
+        console.log("Error in signup controller");
+        res.status(500).json({message : "Internal Server Error"});
     }
 }
 
