435e00ded26a92cd34a748c970e643d4c8741285
rakshit0111 <rakshitsingh333@gmail.com>
2025-03-05 01:10:08 +0530

    Routes working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Navbar from './components/navbar'
+import { Routes , Route} from 'react-router-dom'
+import HomePage from "./pages/HomePage" 
+import SignUpPage from "./pages/SignUpPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
+import LoginPage from "./pages/LoginPage"
+
 
 const App = () => {
   return (
-    <div className = "text-red-500">
-      App
+    <div>
+      <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/signup' element={<SignUpPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/settings' element={<SettingsPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+      </Routes>
     </div>
   )
 }
