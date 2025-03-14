132e7ee3edda1345a63f47ca339726d9f11c5cf7
rakshit0111 <rakshitsingh333@gmail.com>
2025-03-15 02:43:04 +0530

    pfp update feature added

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -61,5 +61,21 @@ export const useAuthStore = create((set) => ({
         } catch (error) {
             toast.error(error.response.data.message);
         }
-    }
+    },
+
+    updateProfile : async(data) =>{
+        set({isUpdatingProfile : true});
+
+        try {
+            const res = await axiosInstance.put("/auth/update-profile",data);
+            set({authUser : res.data});
+            toast.success("Profile Picture updated successfully!!")
+        } catch (error) {
+            console.log("Error in updateProfile of authStore");
+            toast.error(error.response.data.message);
+        }
+        finally{
+            set({isUpdatingProfile : false});
+        }
+    },
 }));
