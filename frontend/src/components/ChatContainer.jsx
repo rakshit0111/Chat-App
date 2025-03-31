620d0158f7de3326ac74eb057103c566fcd3cd11
rakshit0111 <rakshitsingh333@gmail.com>
2025-03-31 14:26:07 +0530

    chat container changes

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore"
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+
 
 const ChatContainer = () => {
+  const {messages,getMessages,isMessagesLoading,selectedUser} = useChatStore();
+
+  useEffect(() =>{
+    getMessages(selectedUser._id);
+  },[getMessages,selectedUser]);
+
+  if(isMessagesLoading)
+    return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader/>
+      <MessageSkeleton/>
+      <MessageInput/>
+    </div>
+    )
+
+
   return (
-    <div>ChatContainer</div>
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader/>
+
+      <p>
+        Messages ....
+      </p>
+
+      <MessageInput/>
+    </div>
   )
 }
 
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set,get) => ({
     messages : [],
     users : [],
     selectedUser : null,
@@ -34,6 +34,17 @@ export const useChatStore = create((set) => ({
         }
         finally{
             set({isMessagesLoading : false});
+        }
+    },
+
+    // make new entry in message database and update the messages state in UI 
+    sendMessage : async(messageData)=>{
+        const {selectedUser,messages} = get();
+        try {
+            const res = await axiosInstance.post(`/message/send/${selectedUser._id}`,messageData);
+            set({messages : [...messages,res.data]});    
+        } catch (error) {
+            toast.error(error.response.data.message);
         }
     },
 
