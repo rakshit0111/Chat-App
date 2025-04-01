e958b96b62c2087b2ec78bf1bb69e345640d9591
rakshit0111 <rakshitsingh333@gmail.com>
2025-04-01 21:34:36 +0530

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,13 @@ import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import {app,server} from "./lib/socket.js"
+import path from "path";
+
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '50mb' }));// json bodyparser
 app.use(cookieParser());//cookie parser middleware 
@@ -22,6 +25,14 @@ app.use(cors({
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 server.listen(PORT,()=>{
     console.log(`Server started on PORT ${PORT}`);
